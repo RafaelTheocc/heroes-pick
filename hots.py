57d2f3a1550f717ee3afddd08a75df65f23eb792
EderDk <eder.ufpi@outlook.com>
2021-08-14 10:10:42 -0300

    Json

--- hots.py
+++ hots.py
@@ -13,20 +13,20 @@ from pprint import pprint
 Mdict = {}
 
 url = [
-    "https://www.heroescounters.com/map/dragonshire" ,
-     "https://www.heroescounters.com/map/alteracpass",
-     "https://www.heroescounters.com/map/battlefieldofeternity",
-     "https://www.heroescounters.com/map/braxisholdout",
-     "https://www.heroescounters.com/map/cursedhollow",
-     "https://www.heroescounters.com/map/gardenofterror",
-     "https://www.heroescounters.com/map/hanamura",
-     "https://www.heroescounters.com/map/infernalshrines",
-     "https://www.heroescounters.com/map/tombofthespiderqueen",
-     "https://www.heroescounters.com/map/towersofdoom",
-     "https://www.heroescounters.com/map/warheadjunction",
-     #"https://www.heroescounters.com/map/blackheartsbay",
-     "https://www.heroescounters.com/map/skytemple"
-     #"https://www.heroescounters.com/map/volskayafoundry"
+    "https://www.heroescounters.com/map/dragonshire",
+    "https://www.heroescounters.com/map/alteracpass",
+    "https://www.heroescounters.com/map/battlefieldofeternity",
+    "https://www.heroescounters.com/map/braxisholdout",
+    "https://www.heroescounters.com/map/cursedhollow",
+    "https://www.heroescounters.com/map/gardenofterror",
+    "https://www.heroescounters.com/map/hanamura",
+    "https://www.heroescounters.com/map/infernalshrines",
+    "https://www.heroescounters.com/map/tombofthespiderqueen",
+    "https://www.heroescounters.com/map/towersofdoom",
+    "https://www.heroescounters.com/map/warheadjunction",
+    # "https://www.heroescounters.com/map/blackheartsbay",
+    "https://www.heroescounters.com/map/skytemple"
+    # "https://www.heroescounters.com/map/volskayafoundry"
 ]
 
 
@@ -38,26 +38,28 @@ driver = webdriver.Firefox(options=option)
 for mapa in url:
     driver.get(mapa)
     dict = {}
-    x=0
-    map_element = driver.find_element_by_xpath('/html/body/div/div[2]/div[1]/div[2]/div/h1')
+    x = 0
+    map_element = driver.find_element_by_xpath(
+        '/html/body/div/div[2]/div[1]/div[2]/div/h1')
     map_content = map_element.get_attribute('innerHTML')
-    
+
     while True:
-        x+=1
+        x += 1
         try:
-            pers_element = driver.find_element_by_xpath('/html/body/div/div[2]/ul/li['+str(x)+']/div/h3/a')
+            pers_element = driver.find_element_by_xpath(
+                '/html/body/div/div[2]/ul/li['+str(x)+']/div/h3/a')
             personagem_name = pers_element.get_attribute('innerHTML')
 
-            element = driver.find_element_by_xpath('/html/body/div/div[2]/ul/li['+str(x)+']/div/a/span[2]/strong')
-            html_content = element.get_attribute('innerHTML')
-            html_content1 = int(html_content)
-            dict[personagem_name] = [html_content1]
+            element = driver.find_element_by_xpath(
+                '/html/body/div/div[2]/ul/li['+str(x)+']/div/a/span[2]/strong')
+            html_content = int(element.get_attribute('innerHTML'))
+            dict[personagem_name] = [html_content]
         except:
             break
     Mdict[map_content] = dict
 pprint(Mdict, sort_dicts=False)
 js = json.dumps(Mdict)
-fp = open('charmap.json','w')
+fp = open('charmap.json', 'w')
 fp.write(js)
 fp.close()
 driver.quit()
